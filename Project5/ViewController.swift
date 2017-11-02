2f81df8be101e61291a2d0ca4a64b9fb44d2f661
frankwolffnl <frankwolff.nl@gmail.com>
2017-11-02 17:43:52 +0100

    Added text file.

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -4,20 +4,47 @@
 //
 //  Created by Frank Wolff on 02/11/2017.
 //  Copyright © 2017 Frank Wolff. All rights reserved.
-//
+//  Next page to do: https://www.hackingwithswift.com/read/5/2/reading-from-disk-contentsoffile
 
 import UIKit
+import GameplayKit
 
 class ViewController: UITableViewController {
-
+	var allWords = [String]()
+	var usedWords = [String]()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt") {
+			if let startWords = try? String(contentsOfFile: startWordsPath) {
+				allWords = startWords.components(separatedBy: "\n")
+			}
+		} else {
+			allWords = ["silkworm"]
+		}
+		startGame()
+	}
+	
+	func startGame() {
+		allWords = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allWords) as! [String]
+		title = allWords[0]
+		usedWords.removeAll(keepingCapacity: true)
+		tableView.reloadData()
+	}
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return usedWords.count
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+		cell.textLabel?.text = usedWords[indexPath.row]
+		return cell
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
 	}
 
 
