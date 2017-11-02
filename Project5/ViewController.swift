7ebb4391d50777d1ea11a1c9a7954a496f0083a1
frankwolffnl <frankwolff.nl@gmail.com>
2017-11-02 21:41:49 +0100

    Finished project 5 of Hacking With Swift.

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -43,6 +43,9 @@ class ViewController: UITableViewController {
 	func submit(answer: String) {
 		let lowerAnswer = answer.lowercased()
 		
+		let errorTitle: String
+		let errorMessage: String
+		
 		if isPossible(word: lowerAnswer) {
 			if isOriginal(word: lowerAnswer) {
 				if isReal(word: lowerAnswer) {
@@ -50,9 +53,23 @@ class ViewController: UITableViewController {
 					
 					let indexPath = IndexPath(row: 0, section: 0)
 					tableView.insertRows(at: [indexPath], with: .automatic)
+					
+					return
+				} else {
+					errorTitle = "Word not recognised"
+					errorMessage = "You can't just make them up, you know!"
 				}
+			} else {
+				errorTitle = "Word used already"
+				errorMessage = "Be more original!"
 			}
+		} else {
+			errorTitle = "Word not possible"
+			errorMessage = "You can't spell that word from '\(title!.lowercased())'!"
 		}
+		let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+		ac.addAction(UIAlertAction(title: "OK", style: .default))
+		present(ac, animated: true)
 	}
 	
 	func startGame() {
@@ -63,15 +80,29 @@ class ViewController: UITableViewController {
 	}
 	
 	func isPossible(word: String) -> Bool {
+		var tempWord = title!.lowercased()
+		
+		for letter in word {
+			if let pos = tempWord.range(of: String(letter)) {
+				tempWord.remove(at: pos.lowerBound)
+			} else {
+				return false
+			}
+		}
+		
 		return true
 	}
 	
 	func isOriginal(word: String) -> Bool {
-		return true
+		return !usedWords.contains(word)
 	}
 	
 	func isReal(word: String) -> Bool {
-		return true
+		let checker = UITextChecker()
+		let range = NSMakeRange(0, word.utf16.count)
+		let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+		
+		return misspelledRange.location == NSNotFound
 	}
 	
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
